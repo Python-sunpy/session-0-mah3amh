840601ddaff1601c23f8128cb8575a1572d62a3a
F-Niazi <131948161+F-Niazi@users.noreply.github.com>
2025-03-02 09:40:43 +0330

    Update homework.py

--- assignments/homework.py
+++ assignments/homework.py
@@ -1,3 +1,13 @@
-def add(a, b):
-    """این تابع باید دو عدد را با هم جمع کند."""
-    pass  # دانشجو باید این قسمت را کامل کند
+# def add(a, b):
+#     """این تابع باید دو عدد را با هم جمع کند."""
+#     pass  # دانشجو باید این قسمت را کامل کند
+
+
+number1 = int(input("enter number 1 : ".title()))
+number2 = int(input("enter number 2 :".title()))
+def add(number1, number2):
+    sum =  number1+ number2
+    print("sum is : " , sum)
+
+
+add(number1 , number2)
