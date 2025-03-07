682e00558de72981367fb877724ef8171f4f5323
mah3amh <mehrbakhsh702@gmail.com>
2025-03-07 15:21:58 +0330

    first exercise from Mahsa

--- assignments/homework.py
+++ assignments/homework.py
@@ -1,3 +1,28 @@
-def add(a, b):
-    """این تابع باید دو عدد را با هم جمع کند."""
-    pass  # دانشجو باید این قسمت را کامل کند
+#Mahsa-Mehrbakhsh-first-exercise
+def add():
+    a1 = int(input("enter your number?".title()))
+    b1 = int(input("enter your number?".title()))
+    z1 = (a1 + b1)
+    return (z1)
+result = add()
+print("result: ",result)
+
+print("***") 
+
+def add(a=2,b=3):
+    z = (a+ b)
+    return (z)
+result = add()
+print("result: " , result)
+
+def add1(c=-1,d=1):
+    x = (c + d)
+    return(x)
+result = add1()
+print("result: ",result)
+
+def add2(e=0,f=0):
+    s = (e + f)
+    return (s)
+result = add2()
+print("result: ",result)
